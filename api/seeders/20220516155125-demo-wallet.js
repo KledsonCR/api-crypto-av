8c684f0aca93ebeccb7c29f825b5c545b0be4ec4
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-16 13:03:06 -0300

    feat: populating wallet table

---
+++ api/seeders/20220516155125-demo-wallet.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('wallets', 
+    [
+      {				
+                name: 'Sandra Gomes',
+				cpf: '058.606.543-56',
+				birthdate: '1999-05-06',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Paula Morais',
+				cpf: '059.607.544-57',
+				birthdate: '2003-01-10',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Sergio Lopes',
+				cpf: '060.608.545-58',
+				birthdate: '2000-02-11',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			   
+      }
+    ],     
+ {});
+},
+
+  async down (queryInterface, Sequelize) {   
+    await queryInterface.bulkDelete('wallets', null, {});
+     
+  }
+};
